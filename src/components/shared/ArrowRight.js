eaaefcf30ed247d5569691bff1c744554a82e16b
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-21 17:12:36 +0500

    add icons

---
+++ src/components/shared/ArrowRight.js
@@ -0,0 +1,7 @@
+export default function ArrowRight() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="black">
+      <path d="M5 12H19M19 12L13 18M19 12L13 6" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+)
+}
