7b90639a560d4a68c38426bdeaf0a54bd9b3bb20
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-21 17:10:55 +0500

    create react app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { Route, Routes } from 'react-router-dom';
+
+import Main from "./pages/Main";
+import AboutUs from './pages/AboutUs';
+import Contacts from './pages/Contacts';
+import NotFound from './pages/NotFound';
+
+import MonitoringPerson from './pages/MonitoringPerson';
+import OKR from './pages/OKR';
+import MonitoringLines from './pages/MonitoringLines';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/contacts" element={<Contacts />} />
+      <Route path="/monitoring-person" element={<MonitoringPerson />} />
+      <Route path="/okr" element={<OKR />} />
+      <Route path="/monitoring-lines" element={<MonitoringLines />} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+  );
+}
+
+export default App;
