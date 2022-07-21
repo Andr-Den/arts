7cb8e451185a3a627d34e6cbef0f6c74eee92482
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-21 17:15:54 +0500

    add page Main

---
+++ src/pages/Main.js
@@ -0,0 +1,18 @@
+import Activities from '../components/activities/Activities';
+import Header from '../components/header/Header';
+import Hero from '../components/hero/Hero';
+import Footer from '../components/footer/Footer';
+
+
+function Main() {
+  return (
+    <div className="page">
+      <Header />
+      <Hero />
+      <Activities />
+      <Footer />
+    </div>
+  );
+}
+
+export default Main;
