93eb8e6146cbf982854c23527a733dbca738fc37
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-21 17:13:00 +0500

    add component Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import logo from '../../images/logo_1.svg'
+import burger from '../../images/menu.svg'
+
+const navMenu = [
+  {
+    name: 'О нас',
+    to: '/about-us'
+  },
+  {
+    name: 'Продукты',
+    to: '/products'
+  },
+  {
+    name: 'Контакты',
+    to: '/contacts'
+  },
+  {
+    name: 'Новости',
+    to: '/news'
+  }
+  , 'Продукты', 'Контакты', 'Новости'
+]
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <a href="/"><img src={logo} alt="логотип" className="header__logo" /></a>
+        <nav className="header__nav">
+          {navMenu.map(({name, to}) => (
+            <a href={to} className="header__links" key={name}>{name}</a>
+          ))}
+        </nav>
+        <img className="header__burger" src={burger} alt=""/>
+      </div>
+    </header>
+  )
+}
+
+export default Header;
