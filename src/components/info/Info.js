2b92539cb110d0af724b5453694600f0faec0377
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-21 17:15:42 +0500

    add component Info

---
+++ src/components/info/Info.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Info() {
+  return (
+    <section className="page">
+      <div className="info__container">
+        <p className="info__phone">+7 (922) 202-24-76</p>
+        <div className="info__box">
+          <p className="info__address">620073, г. Екатеринбург, ул. Крестинского, 44</p>
+          <p className="info__email">info@example.com</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Info;
