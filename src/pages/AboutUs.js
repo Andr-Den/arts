062ed156043c94627aef4f4105235fb5f5d7e972
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-21 17:15:14 +0500

    add page AboutUs

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,15 @@
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+
+
+function AboutUs() {
+  return (
+    <div className="page">
+      <Header />
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default AboutUs;
