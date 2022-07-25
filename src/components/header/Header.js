780bbe42dfd28ec5bae12dc912df91bbd3085152
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-25 20:37:08 +0500

    change color theme in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../images/logo_1.svg'
+import logo from '../../images/logo_verde.svg'
 import burger from '../../images/menu.svg'
 
 const navMenu = [
@@ -25,7 +25,10 @@ function Header() {
   return (
     <header className="header">
       <div className="header__container">
+        <div className="header__logo-box">
         <a href="/arts"><img src={logo} alt="логотип" className="header__logo" /></a>
+        <p className="header__name">НПП "АРТС"</p>
+        </div>
         <nav className="header__nav">
           {navMenu.map(({name, to}) => (
             <a href={to} className="header__links" key={name}>{name}</a>
