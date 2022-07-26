9fdd870841ee247f54d6d16ae3f9d3d221e74441
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-07-26 20:09:05 +0500

    add styles for product component

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function Product({src, title, description}) {
   return (
-    <section>
-      <img src={src} alt="" className="activities__image"/>
-      <div className="activities__text">
-        <h2 className="activities__title">{title}</h2>
-        <p className="activities__description">{description}</p>
+    <section className="product">
+      <div className="product__container">
+      <h2 className="product__title">{title}</h2>
+      <div className="product__box">
+        <img src={src} alt="" className="product__image"/>
+        <p className="product__description">{description}</p>
+      </div>
       </div>
     </section>
   )
